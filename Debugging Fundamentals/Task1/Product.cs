1f9aa53b07dab2c717c4934fc6039158fb0540af
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-05-03 14:56:32 +0300

    Requested changes applied #1

--- Debugging Fundamentals/Task1/Product.cs
+++ Debugging Fundamentals/Task1/Product.cs
@@ -12,16 +12,36 @@
 
         public double Price { get; set; }
 
-        public override bool Equals(object obj)
+        public override bool Equals(System.Object obj)
         {
-            if (obj == null || GetType() != obj.GetType())
+            // If parameter is null return false.
+            if (obj == null)
             {
                 return false;
             }
 
-            Product other = (Product)obj;
-            return Name == other.Name && Price == other.Price;
+            // If parameter cannot be cast to Product return false.
+            Product product = obj as Product;
+            if ((System.Object)product == null)
+            {
+                return false;
+            }
 
+            // Return true if the fields match:
+            return (Name == product.Name) && (Price == product.Price);
+
+        }
+
+        public bool Equals(Product product)
+        {
+            // If parameter is null return false:
+            if ((object)product == null)
+            {
+                return false;
+            }
+
+            // Return true if the fields match:
+            return (Name == product.Name) && (Price == product.Price);
         }
 
         public override int GetHashCode()
--- Debugging Fundamentals/Task1/Utilities.cs
+++ Debugging Fundamentals/Task1/Utilities.cs
@@ -11,7 +11,6 @@ namespace Task1
         public static void Sort(int[] numbers)
         {
             int temp;
-            //_ = numbers ?? throw new ArgumentNullException(nameof(numbers));
             if (numbers == null)
                 throw new ArgumentNullException(nameof(numbers));
             for (int i = 0; i < numbers.Length; i++)
@@ -38,8 +37,6 @@ namespace Task1
         /// otherwise -1.</returns>
         public static int IndexOf(Product[] products, Predicate<Product> predicate)
         {
-            //_ = products ??  throw new ArgumentNullException(nameof(products));
-            //_ = predicate ?? throw new ArgumentNullException(nameof(predicate));
             if(products == null || predicate == null)
                 throw new ArgumentNullException();
             for (int i = 0; i < products.Length; i++)
