162279e04489be53da6246746df0dd61e91c22ee
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-05-18 02:35:04 +0300

    Initial Project

---
+++ AdvancedC#/AdvancedC#-Task1/FileSystemVisitor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedC__Task1
+{
+    public class FileSystemVisitor
+    {
+
+        public void ProcessDirectory(string path)
+        {
+            string[] files = Directory.GetFiles(path);
+            foreach (string file in files)
+            {
+                ProcessFile(file);
+            }
+        }
+
+        public void ProcessFile(string path)
+        {
+            Console.WriteLine("Processed file '{0}.'", path);
+        }
+    }
+}
---
+++ AdvancedC#/AdvancedC#-Task1/Program.cs
@@ -0,0 +1,5 @@
+﻿using AdvancedC__Task1;
+
+FileSystemVisitor fileSystemVisitor = new();
+fileSystemVisitor.ProcessDirectory("C:\\csv");
+Console.ReadLine();
