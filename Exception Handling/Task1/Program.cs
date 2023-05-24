fbf4ef984808714a85e9685827fe373c21eb875f
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-05-24 18:58:26 +0300

    Exception Handling Home Task

---
+++ Exception Handling/Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                PrintFirstCharacter();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+
+        public static void PrintFirstCharacter()
+        {
+            Console.Write("Input: ");
+
+            string input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new ArgumentNullException(nameof(input), "Input cannot be null or white space.");
+            }
+
+            if (string.IsNullOrEmpty(input))
+            {
+                throw new ArgumentException("Input cannot be empty.");
+            }
+
+            Console.WriteLine($"First character of the input: {input[0]}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exception Handling/Task2/NumberParser.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Task2
+{
+    public class NumberParser : INumberParser
+    {
+        public int Parse(string stringValue)
+        {
+            if (stringValue == null)
+            {
+                throw new ArgumentNullException("Value cannot be null or empty.");
+            }
+
+            if (stringValue == string.Empty)
+            {
+                throw new FormatException("Value contains non-numeric characters.");
+            }
+
+            if (!string.IsNullOrWhiteSpace(stringValue))
+            {
+                stringValue = stringValue.Trim();
+            }
+
+            int result = 0;
+            bool isNegative = false;
+            int startIndex = 0;
+
+            if (stringValue[0] == '-')
+            {
+                isNegative = true;
+                startIndex = 1;
+            }
+            else if (stringValue[0] == '+')
+            {
+                startIndex = 1;
+            }
+
+            if (isNegative)
+            {
+                result = -result;
+            }
+
+            for (int i = startIndex; i < stringValue.Length; i++)
+            {
+                char currentChar = stringValue[i];
+
+                if (currentChar < '0' || currentChar > '9')
+                {
+                    throw new FormatException("Value contains non-numeric characters.");
+                }
+
+                int currentDigit = currentChar - '0';
+
+                if (!isNegative)
+                {
+                    result = checked(result * 10 + currentDigit);
+                }
+                else
+                {
+                    result = checked(result * 10 - currentDigit);
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
