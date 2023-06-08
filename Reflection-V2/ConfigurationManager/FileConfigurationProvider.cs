f778dd3a16e6e4f5b20c7e601559846ea4587939
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-06-09 02:32:30 +0300

    Reflection Home Task

---
+++ Reflection-V2/ConfigurationManager/FileConfigurationProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+using Reflection_V2;
+
+namespace ConfigurationManager
+{
+	public class FileConfigurationProvider : Reflection_V2.IConfigurationProvider
+    {
+        private readonly IConfigurationRoot configuration;
+
+        public FileConfigurationProvider()
+		{
+            configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("customconfig.json")
+            .Build();
+        }
+
+        public object GetSetting(string key, Type type)
+        {
+            Console.WriteLine("I am file provider get");
+            return configuration[key];
+        }
+
+        public void SetSetting(string key, string value, Type type)
+        {
+            Console.WriteLine("I am file provider set");
+        }
+    }
+}
+
