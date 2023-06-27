084408424d1de970d5d202ef9603c893ef72852b
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-06-28 01:09:26 +0300

    Update StringSumUtilityTests.cs
    Unit tests for small numbers and non natural number inputs.

--- Unit Tesiting/UnitTestingTests/StringSumUtilityTests.cs
+++ Unit Tesiting/UnitTestingTests/StringSumUtilityTests.cs
@@ -12,6 +12,24 @@ namespace UnitTestingTests
 
             Assert.Equal("0", result);
         }
+
+        [Fact]
+        public void Sum_SmallNumbers_ReturnsSum()
+        {
+            string result = StringSumUtility.Sum("1", "2");
+
+            Assert.Equal("3", result);
+        }
+
+        [Fact]
+        public void Sum_NonNaturalNumbers_ReplaceNonNaturalNumbersWithZero()
+        {
+            string result1 = StringSumUtility.Sum("-5", "2");
+            string result2 = StringSumUtility.Sum("abc", "25");
+
+            Assert.Equal("2", result1);
+            Assert.Equal("25", result2);
+        }
     }
 }
 
