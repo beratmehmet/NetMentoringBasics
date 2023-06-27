dbdc63f3f60cedbdeb97f77b22abad1e40cd15c4
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-06-28 01:46:11 +0300

    Create OddEven.cs
    Implementing basic scenario that return numbers from 1 to 100.

---
+++ Unit Tesiting/Unit Tesiting/OddEven.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Unit_Tesiting
+{
+	public static class OddEven
+	{
+		public static IEnumerable<string> GetNumberType()
+		{
+			for (int i = 1; i <= 100; i++)
+			{
+				yield return i.ToString();
+			}
+		}
+	}
+}
+
