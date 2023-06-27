ddd43b28f04a4ac2bfcdcccfabe9ae2d67180740
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-06-27 23:22:35 +0300

    Base Project

---
+++ Unit Tesiting/Unit Tesiting/RecentlyUsedList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Unit_Tesiting
+{
+	public class RecentlyUsedList
+	{
+        private readonly int? capacity;
+        private readonly LinkedList<string> items;
+        public int Count => items.Count;
+
+        public RecentlyUsedList()
+		{
+            this.capacity = capacity;
+            items = new LinkedList<string>();
+        }
+	}
+}
+
---
+++ Unit Tesiting/UnitTestingTests/RecentlyUsedListTests.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace UnitTestingTests
+{
+	public class RecentlyUsedListTests
+	{
+		
+	}
+}
+
