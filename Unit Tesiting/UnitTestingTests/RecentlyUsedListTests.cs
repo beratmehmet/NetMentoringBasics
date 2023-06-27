2d235e5ea3f00cfa05bbee9364ecdeea71628971
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-06-27 23:33:10 +0300

    Unit Test for Add method
    Testing the RecentlyUsedList's Add method to see if the count is increasing after adding items.

--- Unit Tesiting/UnitTestingTests/RecentlyUsedListTests.cs
+++ Unit Tesiting/UnitTestingTests/RecentlyUsedListTests.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using Unit_Tesiting;
+
 namespace UnitTestingTests
 {
 	public class RecentlyUsedListTests
 	{
-		
-	}
+        [Fact]
+        public void Add_Items_CountIncreased()
+        {
+            var list = new RecentlyUsedList();
+
+            list.Add("item1");
+            list.Add("item2");
+
+            Assert.Equal(2, list.Count);
+        }
+    }
 }
 
