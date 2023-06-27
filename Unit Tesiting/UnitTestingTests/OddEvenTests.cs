aefcc3ca52fe5aadb410a767dd3ddffba8171116
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-06-28 01:45:33 +0300

    Create OddEvenTests.cs
    Unit tests for basic scenarios

---
+++ Unit Tesiting/UnitTestingTests/OddEvenTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Unit_Tesiting;
+
+namespace UnitTestingTests
+{
+	public class OddEvenTests
+	{
+        [Fact]
+        public void GetNumberType_Count()
+        {
+            IEnumerable<string> numberType = OddEven.GetNumberType();
+
+            Assert.Equal(100, numberType.Count());
+        }
+
+        [Fact]
+        public void GetNumberType_Values()
+        {
+            IEnumerable<string> numberType = OddEven.GetNumberType();
+            List<int> expectedValuesInt = new List<int>();
+            expectedValuesInt.AddRange(Enumerable.Range(1, 100));
+            List<string> expectedValuesStr = expectedValuesInt.ConvertAll<string>(x => x.ToString());
+            
+            Assert.Equal(expectedValuesStr, numberType.ToList());
+        }
+    }
+}
+
