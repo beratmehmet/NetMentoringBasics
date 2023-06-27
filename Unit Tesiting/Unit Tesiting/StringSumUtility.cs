1b5f9b469684f8d080c6742c355fab15aca80a70
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-06-28 00:55:53 +0300

    Base implementation for StringSumUtility and Empty string unit test

---
+++ Unit Tesiting/Unit Tesiting/StringSumUtility.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Unit_Tesiting
+{
+	public static class StringSumUtility
+	{
+		public static string Sum(string num1, string num2)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
+
---
+++ Unit Tesiting/UnitTestingTests/StringSumUtilityTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Unit_Tesiting;
+
+namespace UnitTestingTests
+{
+	public class StringSumUtilityTests
+	{
+        [Fact]
+        public void Sum_EmptyString_ReturnsZero()
+        {
+            string result = StringSumUtility.Sum("", "");
+
+            Assert.Equal("0", result);
+        }
+    }
+}
+
