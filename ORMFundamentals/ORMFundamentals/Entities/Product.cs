036dadcacadc73b06ac88f831bb7792a4ca556cb
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-08-10 00:07:11 +0300

    Product.cs format fix and toString() method

--- ORMFundamentals/ORMFundamentals/Entities/Product.cs
+++ ORMFundamentals/ORMFundamentals/Entities/Product.cs
@@ -9,11 +9,22 @@ namespace ORMFundamentals.Entities
     public class Product
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
+
         public string Description { get; set; }
+
         public double Weight { get; set; }
+
         public double Height { get; set; }
+
         public double Width { get; set; }
+
         public double Length { get; set; }
+
+        public override string? ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Description: {Description}, Weight: {Weight}, Height: {Height}, Width: {Width}, Length: {Length}";
+        }
     }
 }
