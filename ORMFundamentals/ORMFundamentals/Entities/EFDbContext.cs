db5693b1746e6c9dc0f7016a644ca74b2e5280c4
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-08-09 23:33:00 +0300

    Base Project (EF)

---
+++ ORMFundamentals/ORMFundamentals/Entities/EFDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORMFundamentals.Entities
+{
+    internal class EFDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=ORMFundamentalsDb;"
+                + "Trusted_Connection=True;");
+        }
+    }
+}
