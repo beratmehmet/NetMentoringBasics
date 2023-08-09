11ee0e8287f6396aca287a1f0738b43700baa0d2
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-08-09 23:33:21 +0300

    Models

---
+++ ORMFundamentals/ORMFundamentals/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORMFundamentals.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public Product product { get; set; }
+    }
+}
