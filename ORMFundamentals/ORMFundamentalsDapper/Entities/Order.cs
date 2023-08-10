3bdd59335e48f485df4a7959c36b54c5e556f6c2
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-08-11 00:39:47 +0300

    Order Model Dapper

--- ORMFundamentals/ORMFundamentalsDapper/Entities/Order.cs
+++ ORMFundamentals/ORMFundamentalsDapper/Entities/Order.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dapper.Contrib.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,28 @@ namespace ORMFundamentalsDapper.Entities
 {
     public class Order
     {
+        public int Id { get; set; }
+
+        public string Status { get; set; }
+
+        [Computed]
+        public DateTime CreatedDate 
+        { 
+            get 
+            {
+                return DateTime.Now;
+            }
+        }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public int ProductId { get; set; }
+
+        public Product Product { get; set; }
+
+        public override string? ToString()
+        {
+            return $"ID: {Id}, Status: {Status}, CreatedDate: {CreatedDate}, UpdatedDate: {UpdatedDate},\n\t Product: {Product}";
+        }
     }
 }
