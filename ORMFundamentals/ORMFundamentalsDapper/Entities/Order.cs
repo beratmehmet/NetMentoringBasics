a7a060396c8fda862e8fb461508985b6fef76c14
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-08-10 22:48:11 +0300

    Product and Order models

---
+++ ORMFundamentals/ORMFundamentalsDapper/Entities/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORMFundamentalsDapper.Entities
+{
+    public class Order
+    {
+    }
+}
