4bf97f6c4d575ee5e3b45fbe4ee767da7854ffb2
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-08-16 01:22:36 +0300

    Category and Product Repository interfaces

---
+++ MVCPrinciples/MVCPrinciples/Models/IProductRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace MVCPrinciples.Models
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> LimitedProducts(int numberOfProducts);
+        void Add(Product product);
+    }
+}
