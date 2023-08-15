81200cf1f67a5b161a0e7bf53d02aa2c3b6f5a11
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-08-16 00:25:29 +0300

    Create CategoryController.cs

---
+++ MVCPrinciples/MVCPrinciples/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCPrinciples.Models;
+
+namespace MVCPrinciples.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private MvcprinciplesContext _db;
+        public CategoryController(MvcprinciplesContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var categories = _db.Categories.ToArray();
+            return View(categories);
+        }
+    }
+}
