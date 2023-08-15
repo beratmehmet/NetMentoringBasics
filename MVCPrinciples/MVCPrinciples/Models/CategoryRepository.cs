f58cfbd6fe87c435ffe9df06ee793fda245f48fd
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-08-16 01:23:04 +0300

    Category and Product Repository Implementations

---
+++ MVCPrinciples/MVCPrinciples/Models/CategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace MVCPrinciples.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private MvcprinciplesContext _db;
+
+        public CategoryRepository(MvcprinciplesContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Category> GetCategories
+        {
+            get
+            {
+                return _db.Categories.ToArray();
+            }
+        }
+    }
+}
---
+++ MVCPrinciples/MVCPrinciples/Models/ProductRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Runtime;
+
+namespace MVCPrinciples.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+        private MvcprinciplesContext _db;
+
+        public ProductRepository(MvcprinciplesContext db)
+        {
+            _db = db;
+        }
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> LimitedProducts(int numberOfProducts)
+        {
+            IEnumerable<Product> products;
+            if (numberOfProducts > 0)
+            {
+                products = _db.Products.Include(p => p.Supplier).Include(p => p.Category).Take(numberOfProducts).ToList();
+            }
+            else
+            {
+                products = _db.Products.Include(p => p.Supplier).Include(p => p.Category).ToList();
+            }
+            return products;
+        }
+    }
+}
