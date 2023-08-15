1bbe0124c04ca3b99aba2dd9d2daf397d48f890b
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-08-16 00:26:25 +0300

    Create ProductController.cs

---
+++ MVCPrinciples/MVCPrinciples/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVCPrinciples.Models;
+
+namespace MVCPrinciples.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private MvcprinciplesContext _db;
+
+        public ProductController(MvcprinciplesContext db)
+        {
+            _db = db;
+            
+        }
+
+        public IActionResult Index()
+        {
+            var products = _db.Products.Include(p => p.Supplier).Include(p => p.Category).ToList();
+            return View(products);
+        }
+    }
+}
