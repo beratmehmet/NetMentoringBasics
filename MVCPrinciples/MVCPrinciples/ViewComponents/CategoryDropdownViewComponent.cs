7d44c050381f956fb2138bb67f8de3b66f64b149
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-08-16 22:47:20 +0300

    Category and Supplier dropdown view components

---
+++ MVCPrinciples/MVCPrinciples/ViewComponents/CategoryDropdownViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using MVCPrinciples.Models;
+
+namespace MVCPrinciples.ViewComponents
+{
+	public class CategoryDropdownViewComponent : ViewComponent
+	{
+		private readonly MvcprinciplesContext _db;
+
+		public CategoryDropdownViewComponent(MvcprinciplesContext db)
+		{
+			_db = db;
+		}
+
+        public IViewComponentResult Invoke()
+        {
+			var categories = _db.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
+
---
+++ MVCPrinciples/MVCPrinciples/ViewComponents/SupplierDropdownViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using MVCPrinciples.Models;
+
+namespace MVCPrinciples.ViewComponents
+{
+	public class SupplierDropdownViewComponent : ViewComponent
+	{
+		private readonly MvcprinciplesContext _db;
+
+		public SupplierDropdownViewComponent(MvcprinciplesContext db)
+		{
+			_db = db;
+		}
+
+        public IViewComponentResult Invoke()
+        {
+            var suppliers = _db.Suppliers.ToList();
+            return View(suppliers);
+        }
+    }
+}
+
