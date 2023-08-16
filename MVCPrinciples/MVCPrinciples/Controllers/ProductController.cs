f9a6d64cca085d01e68ff67502e1ec34da537def
Berat Mehmet Topuz <beratmehmetpz@gmail.com>
2023-08-16 22:48:20 +0300

    Create and Edit routes for Product Controller

--- MVCPrinciples/MVCPrinciples/Controllers/ProductController.cs
+++ MVCPrinciples/MVCPrinciples/Controllers/ProductController.cs
@@ -8,11 +8,10 @@ namespace MVCPrinciples.Controllers
     public class ProductController : Controller
     {
         private readonly SettingsModel _settings;
-
         private readonly IProductRepository _productRepository;
+
         public ProductController(IOptions<SettingsModel> options, IProductRepository productRepository)
         {
-
             _settings = options.Value;
             _productRepository = productRepository;
         }
@@ -20,7 +19,48 @@ namespace MVCPrinciples.Controllers
         public IActionResult Index()
         {
             IEnumerable<Product> products = _productRepository.LimitedProducts(_settings.NumberOfProducts);
+
             return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            _productRepository.Add(product);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var product = _productRepository.GetProductById(id);
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            _productRepository.Edit(product);
+
+            return RedirectToAction("Index");
         }
     }
 }
